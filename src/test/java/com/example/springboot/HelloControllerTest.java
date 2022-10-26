133bff93c5ae6c7236d165c367c44f088f8901eb
arcaplay <suppakiat.p@gmail.com>
2022-10-26 12:22:47 +0700

    change test cond. to Demo1

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu! + Demo", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu! + Demo1", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! + Demo"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu! + Demo1"));
     }
 }
